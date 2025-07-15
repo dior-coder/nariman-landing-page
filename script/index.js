2d7be378fec994c7ee17510c50f323cf2dd2df99
Dior Coder <109025606+dior-coder@users.noreply.github.com>
2025-07-16 00:13:30 +0600

    update

--- script/index.js
+++ script/index.js
@@ -1,2 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+  // Скрипт для кнопок прокрутки (опционально)
+  document.querySelector('.scroll-left').addEventListener('click', function() {
+    document.querySelector('.reviews-scroll').scrollBy({
+      left: -300,
+      behavior: 'smooth'
+    });
+  });
+  
+  document.querySelector('.scroll-right').addEventListener('click', function() {
+    document.querySelector('.reviews-scroll').scrollBy({
+      left: 300,
+      behavior: 'smooth'
+    });
+  });
